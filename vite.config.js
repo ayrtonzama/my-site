3acd57b2bf0793f04c705c9e46185ce9558a4714
Ayrton Zamarian <ayrtonzama@gmail.com>
2026-05-31 21:13:34 +0200

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// NOTE: set `base` to '/<your-repo-name>/' for a project site,
-// e.g. base: '/portfolio/'. If you deploy to <username>.github.io
-// (a user site) instead, set base: '/'.
 export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio/',
-})
+  plugins: [react()]
+});
